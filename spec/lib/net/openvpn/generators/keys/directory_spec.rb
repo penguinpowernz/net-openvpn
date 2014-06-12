38874767b0f3652a4841b849cf7c93ab4c8f7d98
Robert McLeod <robert@autogrow.com>
2014-06-12 13:07:35 +1200

    add passing specs for key dir generator

---
+++ spec/lib/net/openvpn/generators/keys/directory_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe Net::Openvpn::Generators::Keys::Directory, fakefs: true do
+  subject(:dir) { Net::Openvpn::Generators::Keys::Directory.new(props) }
+  let(:props)   { {} }
+
+  before(:each) { setup_filesystem(:fake) }
+
+  it "should make setup? true after setting up" do
+    dir.setup
+    expect(dir).to be_setup
+  end
+
+  it "should create the key dir folder" do
+    dir.setup
+    expect(File.exist? "/etc/openvpn/keys").to be_true
+  end
+
+  context "when the key dir property is changed" do
+    let(:props) { { key_dir: "/etc/openvpn/my_keys", key_index: "/etc/openvpn/my_keys/index.txt" } }
+    it "should create the key dir folder" do
+      dir.setup
+      expect(File.exist? "/etc/openvpn/my_keys").to be_true
+    end
+  end
+
+end
